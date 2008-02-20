dc1c6441d087d5821a64e57d470cc1a60ee9d47e
ryan.andrew.hendrickson <ryan.andrew.hendrickson@5f1b0cec-7246-0410-98ea-39ccbde23240>
2008-02-20 09:48:26 +0000

    logger class.  This enables us to write to files using a delegate.

---
+++ trunk/marvin_cs/logger.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Marvin_cs;
+using System.IO;
+
+namespace Marvin_cs
+{
+    public class logger
+    {
+        FileStream fileStream;
+        StreamWriter streamWriter;
+        public delegate void LogHandler(string message);
+
+        // Constructor
+        public logger(string filename)
+        {
+            fileStream = new FileStream(filename, FileMode.Create);
+            streamWriter = new StreamWriter(fileStream);
+        }
+
+        // Member Function which is used in the Delegate
+        public void Log(string s)
+        {
+            Console.WriteLine(s);
+            streamWriter.WriteLine(s);
+            ///Close();
+        }
+
+        public void Close()
+        {
+            streamWriter.Close();
+            fileStream.Close();
+        }
+    }
+}
